b076dea4d21392d31b90f502ce7163d077b24c62
吴迪 <wudi@hetao101.com>
2018-11-24 16:54:52 +0800

    add casbin

--- enforcer/casbin.go
+++ enforcer/casbin.go
@@ -1,1 +1,12 @@
 package enforcer
+
+import (
+	"github.com/casbin/casbin"
+	"github.com/casbin/gorm-adapter"
+)
+
+func NewCasbinEnforcer(connStr string) *casbin.Enforcer {
+	Adapter := gormadapter.NewAdapter("mysql", connStr, true)
+	enforcer := casbin.NewEnforcer(casbin.NewModel(CasbinConf), Adapter)
+	return enforcer
+}
--- enforcer/casbinMv.go
+++ enforcer/casbinMv.go
@@ -1,1 +1,105 @@
+//Based on https://github.com/labstack/echo-contrib/casbin
+
+//License
+
+//The MIT License (MIT)
+//
+//Copyright (c) 2017 LabStack
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 package enforcer
+
+import (
+	"github.com/Soontao/go-simple-api-gateway/key"
+	"github.com/casbin/casbin"
+	"github.com/hb-go/echo-web/middleware/session"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	// Config defines the config for CasbinAuth middleware.
+	Config struct {
+		// Skipper defines a function to skip middleware.
+		Skipper middleware.Skipper
+
+		// Enforcer CasbinAuth main rule.
+		// Required.
+		Enforcer *casbin.Enforcer
+	}
+)
+
+var (
+	// DefaultConfig is the default CasbinAuth middleware config.
+	DefaultConfig = Config{
+		Skipper: middleware.DefaultSkipper,
+	}
+)
+
+// Middleware returns a CasbinAuth middleware.
+//
+// For valid credentials it calls the next handler.
+// For missing or invalid credentials, it sends "401 - Unauthorized" response.
+func Middleware(ce *casbin.Enforcer) echo.MiddlewareFunc {
+	c := DefaultConfig
+	c.Enforcer = ce
+	return MiddlewareWithConfig(c)
+}
+
+// MiddlewareWithConfig returns a CasbinAuth middleware with config.
+// See `Middleware()`.
+func MiddlewareWithConfig(config Config) echo.MiddlewareFunc {
+	// Defaults
+	if config.Skipper == nil {
+		config.Skipper = DefaultConfig.Skipper
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) || config.CheckPermission(c) {
+				return next(c)
+			} else {
+				return echo.ErrForbidden
+			}
+		}
+	}
+}
+
+// GetUserName gets the user name from the request.
+// Currently, only HTTP basic authentication is supported
+func (a *Config) GetUserName(c echo.Context) (username string) {
+	sess := session.Default(c)
+	tmp := sess.Get(key.KEY_Username)
+	if tmp != nil {
+		username = tmp.(string)
+	} else {
+		username = key.KEY_CasbinAnonymous
+	}
+	return
+}
+
+// CheckPermission checks the user/method/path combination from the request.
+// Returns true (permission granted) or false (permission forbidden)
+func (a *Config) CheckPermission(c echo.Context) bool {
+	user := a.GetUserName(c)
+	method := c.Request().Method
+	path := c.Request().URL.Path
+	return a.Enforcer.Enforce(user, path, method)
+}
